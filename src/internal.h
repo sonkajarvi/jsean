c114e3ad00f3603dbce2e64a14c5b6fa31666abf
sonkajarvi <sonkajarvi@hotmail.com>
2025-09-09 02:54:16 +0300

    add internal.h
    Forgot to add this. :)

---
+++ src/internal.h
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2025, sonkajarvi
+//
+// SPDX-License-Identifier: MIT
+//
+
+#ifndef INTERNAL_H
+#define INTERNAL_H
+
+#include "jsean/jsean.h"
+
+void free_object(jsean *json);
+void free_array(jsean *json);
+
+#endif // INTERNAL_H
